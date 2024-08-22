fad40cf5870834fdb8adc52225536f5dcf586e0f
codej625 <codej625@gmail.com>
2024-08-23 00:30:22 +0900

    240823 Update nav and tailwind css by codej625

---
+++ portfolio-project/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './assets/common.css'
+import Header from './components/commons/Header';
+import Footer from './components/commons/Footer';
+import Main from './components/pages/Main';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
---
+++ portfolio-project/src/components/pages/Main.tsx
@@ -0,0 +1,10 @@
+// import { useState } from 'react'
+import './../../assets/common.css'
+
+export default function Main() {
+  return (
+    <>
+      Main
+    </>
+  )
+}
