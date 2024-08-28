ea557a17e487d5d5448f781012bc5fd456ab591a
codej625 <codej625@gmail.com>
2024-08-28 23:27:52 +0900

    240828 Update by codej625

--- portfolio-project/src/components/pages/Main.tsx
+++ portfolio-project/src/components/pages/Main.tsx
@@ -11,22 +11,26 @@ export default function Main() {
               <div className="lg:max-w-lg">
                 <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Profile</h1>
                 <p className="mt-6 text-xl leading-8 text-gray-700">
-                  "<b>1명의 개발자가 하나의 기업가치를 갖는다</b>"라는 신념을<br />
+                  "<b>1명의 개발자가 하나의 기업가치를 갖는다</b>"라는 신념을
                   가지고 있는 개발자 이진우입니다.<br />
-                  <br />
+                </p>
+                <p className="mt-6 text-xl leading-8 text-gray-700">
                   제가 잘하는 일은 문제를 인식하고,<br />
-                  다양한 역할을 수행하며 해결하는 것입니다.<br />
-                  <br />
+                  다양한 역할을 수행하며 해결하는 것입니다.
+                </p>
+                <p className="mt-6 text-xl leading-8 text-gray-700">
                   요구사항을 파악하여 기술적인 해결책을 제시하고<br />
-                  팀원, 협력부서 더 나아가 클라이언트와의 <br />
-                  원활한 커뮤니케이션 능력을 갖추고 있습니다.<br />
-                  <br />
+                  팀원, 협력부서 더 나아가 클라이언트와의<br />
+                  원활한 커뮤니케이션 능력을 갖추고 있습니다.
+                </p>
+                <p className="mt-6 text-xl leading-8 text-gray-700">
                   항상 겸손한 마음가짐으로 여러 경험을 쌓으며<br />
                   현재도 계속해서 발전해 나가고 있습니다.
                 </p>
               </div>
             </div>
           </div>
+
           <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
             <img
               alt=""
@@ -34,45 +38,32 @@ export default function Main() {
               className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
             />
           </div>
+          
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
-              <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-                <p>
-                  ...
-                </p>
+              <div className="lg:max-w-lg">
+                <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Work Experience</h1>
                 <ul role="list" className="mt-8 space-y-8 text-gray-600">
                   <li className="flex gap-x-3">
+                    <span>2024.05 ~ 재직중</span>
                     <span>
-                      <strong className="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet
-                      consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                      blanditiis ratione.
+                      <strong className="font-semibold text-gray-900">3. 나이콤</strong>
+                      
                     </span>
                   </li>
                   <li className="flex gap-x-3">
+                    <span>2022.01 ~ 2024.04</span>
                     <span>
-                      <strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua
-                      ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                      <strong className="font-semibold text-gray-900">2. 엠플랜잇</strong>
                     </span>
                   </li>
                   <li className="flex gap-x-3">
+                    <span>2017.09 ~ 2021.01</span>
                     <span>
-                      <strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien
-                      vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
+                      <strong className="font-semibold text-gray-900">1. 컴퓨존</strong>
                     </span>
                   </li>
                 </ul>
-                <p className="mt-8">
-                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
-                  fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
-                  adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
-                </p>
-                <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-                <p className="mt-6">
-                  Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
-                  Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-                  tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-                  turpis ipsum eu a sed convallis diam.
-                </p>
               </div>
             </div>
           </div>
