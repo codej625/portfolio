ba043345a9dd8cfbc65e4e5fbc469faa1bbd6b8a
codej625 <codej625@gmail.com>
2024-09-02 03:36:47 +0900

    240902 Update main contents by codej625

--- portfolio-project/src/components/pages/Main.tsx
+++ portfolio-project/src/components/pages/Main.tsx
@@ -6,13 +6,24 @@ export default function Main() {
     <main>
       <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+          
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+            <div className="lg:pr-4">
+              <div className="lg:max-w-lg">
+                <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                  현실에 안주하지 않고,<br />계속해서 발전 중인 개발자<br />이진우입니다.
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:col-span-2 lg:col-start-1 lg:row-start-5 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
               <div className="lg:max-w-lg">
                 <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Profile</h1>
                 <p className="mt-6 text-xl leading-8 text-gray-700">
-                  "<b>1명의 개발자가 하나의 기업가치를 갖는다</b>"라는 신념을
-                  가지고 있는 개발자 이진우입니다.<br />
+                  "<b>뛰어난 1명의 개발자는 하나의 기업가치를 가진다</b>"라는<br />
+                  신념을 갖고 있습니다.
                 </p>
                 <p className="mt-6 text-xl leading-8 text-gray-700">
                   제가 잘하는 일은 문제를 인식하고,<br />
@@ -31,37 +42,51 @@ export default function Main() {
             </div>
           </div>
 
-          <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+          {/* <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
             <img
               alt=""
               src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
               className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
             />
-          </div>
-          
-          <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          </div> */}
+
+          <div className="lg:col-span-2 lg:col-start-1 lg:row-start-9 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
               <div className="lg:max-w-lg">
                 <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Work Experience</h1>
                 <ul role="list" className="mt-8 space-y-8 text-gray-600">
-                  <li className="flex gap-x-3">
-                    <span>2024.05 ~ 재직중</span>
-                    <span>
-                      <strong className="font-semibold text-gray-900">3. 나이콤</strong>
-                      
-                    </span>
+                  <li className="">
+                    <div className="flex">
+                      <span className="span-width-3">2024.05 ~ 재직중</span>
+                      <span>
+                        <strong className="ps-10 font-semibold text-gray-900">나이콤</strong>
+                      </span>
+                    </div>
+                    <p>
+                      contents
+                    </p>
                   </li>
-                  <li className="flex gap-x-3">
-                    <span>2022.01 ~ 2024.04</span>
-                    <span>
-                      <strong className="font-semibold text-gray-900">2. 엠플랜잇</strong>
-                    </span>
+                  <li className="">
+                    <div className="flex">
+                      <span className="span-width-3">2022.01 ~ 2024.04</span>
+                      <span>
+                        <strong className="ps-10 font-semibold text-gray-900">엠플랜잇</strong>
+                      </span>
+                    </div>
+                    <p>
+                      contents
+                    </p>
                   </li>
-                  <li className="flex gap-x-3">
-                    <span>2017.09 ~ 2021.01</span>
-                    <span>
-                      <strong className="font-semibold text-gray-900">1. 컴퓨존</strong>
-                    </span>
+                  <li className="">
+                    <div className='flex'>
+                      <span className="span-width-3">2017.09 ~ 2021.01</span>
+                      <span>
+                        <strong className="ps-10 font-semibold text-gray-900">컴퓨존</strong>
+                      </span>
+                    </div>
+                    <p>
+                      contents
+                    </p>
                   </li>
                 </ul>
               </div>
