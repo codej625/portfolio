554b16a16c73069d407c9ec05f167aa4eaff5a40
codej625 <codej625@gmail.com>
2024-10-26 22:02:31 +0900

    241026 Update Header component by codej625

--- portfolio-project/src/components/commons/Header.tsx
+++ portfolio-project/src/components/commons/Header.tsx
@@ -14,9 +14,12 @@ export default function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#">자기소개</Nav.Link>
-              <Nav.Link href="#">경력사항</Nav.Link>
-              <Nav.Link href="#">프로젝트</Nav.Link>
+              <Nav.Link href="personal-information">인적사항</Nav.Link>
+              <Nav.Link href="self-introduction">자기소개</Nav.Link>
+              <Nav.Link href="work-experience">경력사항</Nav.Link>
+              <Nav.Link href="projects">프로젝트</Nav.Link>
+              <Nav.Link href="skills">보유기술</Nav.Link>
+              <Nav.Link href="others">기타</Nav.Link>
               {/* 
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#">1</NavDropdown.Item>
