909b773ec48271049ec9c08639db60e43128ad9b
codej625 <codej625@gmail.com>
2024-10-27 20:08:05 +0900

    241027 Update Index component by codej625

--- portfolio-project/src/components/commons/Footer.tsx
+++ portfolio-project/src/components/commons/Footer.tsx
@@ -1,9 +1,23 @@
+import Container from "react-bootstrap/Container";
 import './../../assets/common.css'
+
 
 export default function Footer() {
   return (
-    <footer>
-      Footer
+    <footer className="footer">
+      <div className="bg-body-tertiary pt-3">
+        <Container fluid>
+          <div className="row">
+            <div className="col">
+              <div className="footer__title"></div>
+              <div>
+                <p>Email dkwksla@naver.com</p> 
+                <p>Phone <a href="tel:01012345678">010-2269-5901</a></p>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </footer>
   )
 }
--- portfolio-project/src/components/pages/Index.tsx
+++ portfolio-project/src/components/pages/Index.tsx
@@ -1,12 +1,52 @@
 import Header from "./../commons/Header";
 import Footer from "./../commons/Footer";
+import { Container, Card } from 'react-bootstrap';
+
+
+import "./../../assets/common.css";
 
 export default function Main() {
   return (
     <>
       <Header />
       <main>
-        
+        <section id="personal-information">
+          <Container fluid className="pt-5">
+            <div className="row">
+              <div className="col">
+                <Card style={{ width: '15rem' }}>
+                  <Card.Img variant="top" src="./../../public/img.jpg" />
+                  <Card.Body>
+                    <Card.Title className="mx-1">인적사항</Card.Title>
+                    <Card.Text>
+                      <div>
+                        <span className="me-2"><strong>이름</strong></span>
+                        <span>이진우</span>
+                      </div>
+                      <div>
+                        <span className="me-2"><strong>생년월일</strong></span>
+                        <span>91.06.25</span>
+                      </div>
+                      <div>
+                        <span className="me-2"><strong>전화번호</strong></span>
+                        <span>010-2269-5901</span>
+                      </div>
+                      <div>
+                        <span className="me-2"><strong>주소</strong></span>
+                        <span>경기도 군포시 금정동</span>
+                      </div>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            </div>
+          </Container>
+        </section>
+        <section id="self-introduction"></section>
+        <section id="work-experience"></section>
+        <section id="projects"></section>
+        <section id="skills"></section>
+        <section id="others"></section>
       </main>
       <Footer />
     </>
