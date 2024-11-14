19c21f642cc9702628c2025a4200bebf3b68e121
codej625 <codej625@gmail.com>
2024-11-14 15:28:51 +0900

    241114 deploy test by codej625

--- portfolio-project/src/App.tsx
+++ portfolio-project/src/App.tsx
@@ -1,13 +1,17 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Index from "./components/pages/Index";
 
 export default function App() {
+
+  // const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+  
   return (
-    <Router basename="/">
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path={import.meta.env.VITE_APP_BASE_URL} element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+    <Index />
+    // <Router basename="/">
+    //   <Routes>
+    //     <Route path="/" element={<Index />} />
+    //     <Route path={baseUrl} element={<Navigate to="/" replace />} />
+    //   </Routes>
+    // </Router>
   );
 }
